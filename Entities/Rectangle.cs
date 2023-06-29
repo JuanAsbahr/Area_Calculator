44887bcf2a14cd790658b9a7fb7b2ac3233ecac8
JuanAsbahr <juan_asbahr@hotmail.com>
2023-06-29 15:01:15 -0300

    Circle and Rectangle

---
+++ Entities/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using Area_Calculator.Entities.Enums;
+using System.Drawing;
+
+namespace Area_Calculator.Entities
+{
+    internal class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Heigth { get; set; }
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Heigth = height;
+        }
+        public override double Area()
+        {
+            return Width * Heigth;
+        }
+    }
+}
