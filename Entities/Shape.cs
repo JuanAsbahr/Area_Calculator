9ac2f96311c24455cc02897bee61200f47095728
JuanAsbahr <juan_asbahr@hotmail.com>
2023-06-29 14:45:32 -0300

    Shape Class

--- Entities/Shape.cs
+++ Entities/Shape.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Area_Calculator.Entities.Enums;
 
 namespace Area_Calculator.Entities
 {
-    internal class Shape
+    abstract class Shape
     {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
     }
 }
